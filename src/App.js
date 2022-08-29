da5d803e7fc87a50495a3b61f6454ef4754bee12
sunday odum <sundayodum@gmail.com>
2022-08-29 03:22:06 +0100

    structure setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-import React from 'react'
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
+export default App;
